bed29ac44037676c8505a56f29b7c4e80fff9fec
amdy2162 <amdy2162@hotmail.com>
2023-11-10 13:13:51 +0800

    adjust: 新增使用tailwindCss

---
+++ src/components/Footer.vue
@@ -0,0 +1,7 @@
+<script setup>
+
+</script>
+
+<template>
+ <h1 class="text-3xl text-red-500">2222</h1>
+</template>
--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/static-job-listings/',
   plugins: [vue()],
 })
