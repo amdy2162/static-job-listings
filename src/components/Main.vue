d2b8238d8bb2c53dbac69318d0f1fd56c2e0deee
amdy2162 <amdy2162@hotmail.com>
2023-11-10 16:17:40 +0800

    adjust:變更圖片位置

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -2,6 +2,7 @@
 import { ref } from 'vue'
 import data from '../data.json';
 
+
 const jobList = ref(data)
 const secachJob = ref('')
 </script>
@@ -9,12 +10,16 @@ const secachJob = ref('')
 <template>
 <div class="w-full  py-[90px] px-[180px] bg-[#effafa] ">
     <div class="bg-white rounded w-full h-16 sticky z-10 p-4 -mt-[8.3em] shadow-md flex justify-between items-center px-12 font-mono">
-        <input type="text" v-model="secachJob " placeholder="Search you dream job..." class="w-[43%] h-10 border-2 border-none rounded px-4 outline-none focus:border-[#5ba4a4]  text-[#5ba4a4]  placeholder:text-[#5ba4a4]">
+        <!-- <input type="text" v-model="secachJob " placeholder="Search you dream job..." class="w-[43%] h-10 border-2 border-none rounded px-4 outline-none focus:border-[#5ba4a4]  text-[#5ba4a4]  placeholder:text-[#5ba4a4]"> -->
         <div class="text-[#5ba4a4] underline underline-offset-1 font-bold opacity-50">Clear</div>
     </div>
 
     <div class="my-8 bg-white h-36 rounded-md shadow-lg" v-for="item in jobList"> 
-        <img :src="item.logo" alt="" class="w-full h-full">
+        <!-- <img src="../images/eyecam-co.svg" alt="" class="">  -->
+        <div class="">
+            {{ item.company }}
+            <!-- {{ item. }} -->
+        </div>
     </div>
 </div>
 </template>
