e20ca70dcc42154640e15e60d6cc1f9980276e9f
amdy2162 <amdy2162@hotmail.com>
2023-11-13 09:16:36 +0800

    adjust:修改vite參數

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,8 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import copy from 'rollup-plugin-copy'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/static-job-listings/',
-  plugins: [vue(),
-    copy({
-      targets: [
-        { src: 'src/assets/*', dest: 'dist/assets' },
-      ],
-      hook: 'writeBundle' // 確保檔案在寫入 bundle 之後被複製
-    })]
-  ,
+  plugins: [vue()]
 })
